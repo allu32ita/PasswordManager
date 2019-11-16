956ad8ee61de49455c499154953873b190aa2f5f
allu32ita <aluigiandrea@gmail.com>
2019-11-16 13:35:57 +0100

    prova commit

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace PasswordManager
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
