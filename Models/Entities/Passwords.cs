9a42f6ec18b9c025238e3edc772607f71452413d
allu32ita <aluigiandrea@gmail.com>
2019-11-19 20:54:07 +0100

    commit creazione entita con entiti framework

---
+++ Models/Entities/Passwords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordManager.Models.Entities
+{
+    public partial class Passwords
+    {
+        public long Id { get; set; }
+        public string Password { get; set; }
+        public string Descrizione { get; set; }
+        public string DataInserimento { get; set; }
+        public long FkUtente { get; set; }
+        public string Sito { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ Models/Services/Infrastructure/PasswordDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PasswordManager.Models.Entities
+{
+    public partial class PasswordDbContext : DbContext
+    {
+        public PasswordDbContext()
+        {
+        }
+
+        public PasswordDbContext(DbContextOptions<PasswordDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Passwords> Passwords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=Data/dbPassword.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Passwords>(entity =>
+            {
+                entity.HasIndex(e => e.Id)
+                    .IsUnique();
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+        }
+    }
+}
